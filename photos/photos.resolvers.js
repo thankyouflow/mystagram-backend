bc7bc63cde9e37c2522005acd126f7efd77d1b59
유정수 <>
2021-07-06 14:33:39 +0900

    Like

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -13,5 +13,28 @@ export default {
           },
         },
       }),
+    likes: ({ id }) => client.like.count({ where: { photoId: id } }),
+  },
+
+  Hashtag: {
+    photos: async ({ id }, { page }, { loggedInUser }) => {
+      return client.hashtag
+        .findUnique({
+          where: {
+            id,
+          },
+        })
+        .photos();
+    },
+    totalPhotos: ({ id }) =>
+      client.photo.count({
+        where: {
+          hashtags: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
   },
 };
