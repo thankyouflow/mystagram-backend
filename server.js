25188ded1744ffc637ce974ca130b14fa2bede3e
유정수 <>
2021-07-09 17:39:03 +0900

    I am succeser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config()
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
@@ -8,11 +9,20 @@ import {getUser} from "./users/users.utils";
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  context: async ({ req }) => {
+  context: async (ctx) => {
+    let token
+    //http
+    if (ctx.req) {
+      token = ctx.req.headers.token
+    }
+    //websocket 
+    else {
+      token = ctx.connection.context.token
+    }
     return {
-      loggedInUser: await getUser(req.headers.token),
+      loggedInUser: await getUser(token),
     };
-  }
+  },
 });
 
 const PORT = process.env.PORT;
@@ -21,6 +31,10 @@ const app = express();
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`🚀Server is running on http://localhost:${PORT}/graphql ✅`);
 });
